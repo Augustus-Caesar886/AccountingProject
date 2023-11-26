6b8f8d25c9c9fdde72b2b9d738ca1c2f05f96113
Augustus-Caesar886 <williambruce398@gmail.com>
2023-11-26 12:48:42 -0800

    Implement exception throwing for Date

--- src/Date.cpp
+++ src/Date.cpp
@@ -1,11 +1,21 @@
 #include "../header/Date.h"
 
+#include <stdexcept>
+
 using std::to_string;
+using std::invalid_argument;
+using std::stoi;
 
 Date::Date(const string& mmddyyyy) {
-    month = (mmddyyyy[0] - '0') * 10 + (mmddyyyy[1] - '0');
-    day = (mmddyyyy[3] - '0') * 10 + (mmddyyyy[4] - '0');
-    year = (mmddyyyy[6] - '0') * 1000 + (mmddyyyy[7] - '0') * 100 + (mmddyyyy[8] - '0') * 10 + (mmddyyyy[9] - '0');
+    month = stoi(mmddyyyy.substr(0, 2));
+    if(month < 0 or month > 12) throw invalid_argument("invalid month");
+    if(month != (mmddyyyy[0]-'0') * 10 + (mmddyyyy[1]-'0')) throw invalid_argument("bad month input");
+    day = stoi(mmddyyyy.substr(3, 2));
+    if(day < 0 or day > 31) throw invalid_argument("invalid day");
+    if(day != (mmddyyyy[3]-'0') * 10 + (mmddyyyy[4]-'0')) throw invalid_argument("bad day input");
+    year = stoi(mmddyyyy.substr(6, 4));
+    if(year < 0 or year > 9999) throw invalid_argument("invalid year");
+    if(year != (mmddyyyy[6]-'0') * 1000 + (mmddyyyy[7]-'0') * 100 + (mmddyyyy[8]-'0') * 10 + (mmddyyyy[9]-'0')) throw invalid_argument("bad year input");
 }
 
 string Date::stringForm() const {
