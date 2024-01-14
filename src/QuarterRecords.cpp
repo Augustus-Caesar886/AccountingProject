b57e31058dc2acce618d4db1afd237349b9bef2b
Augustus-Caesar886 <williambruce398@gmail.com>
2024-01-13 22:30:02 -0800

    Add future record adjustments for consistency

--- src/QuarterRecords.cpp
+++ src/QuarterRecords.cpp
@@ -9,15 +9,17 @@ QuarterRecords::QuarterRecords(DateUnit year, DateUnit quarter, ValueType valueT
     if(quarter < 1 or quarter > 4) throw invalid_argument("Accounting quarter not within bounds");
 
     for(unsigned i = 1; i <= 3; ++i) {
-        months.push_back(MonthRecords(year, i + 3 * (quarter - 1), valueType, beginningBalance));
+        months.push_back(MonthRecords(year, i + 3 * (quarter - 1), valueType, 0));
     }
+    months[0].setBeginningBalance(beginningBalance);
+    months[0].setEndingBalance(beginningBalance);
 }
 
 void QuarterRecords::addEntry(const LedgerModification& entry) {
     if(entry.getDate().month < (quarter - 1) * 3 + 1 or entry.getDate().month > quarter * 3) throw invalid_argument("Invalid month for Quarter " + to_string(quarter));
 
     AccountRecords::addEntry(entry);
-    if(entry.getDate().month - 3 * (quarter-1) - 1 != 0 and months[entry.getDate().month - 3 * (quarter-1) - 1].getEntries().size() == 0) {
+    if(entry.getDate().month - 3 * (quarter-1) - 1 != 0 and months[entry.getDate().month - 3 * (quarter-1) - 1].getEntries().size() == 0) { //Adjust past records
         if(entry.getDate().month - 3 * (quarter-1) - 2 != 0 and months[entry.getDate().month - 3 * (quarter-1) - 2].getEntries().size() == 0) {
             months[entry.getDate().month - 3 * (quarter-1) - 2].setBeginningBalance(months[0].getEndingBalance());
             months[entry.getDate().month - 3 * (quarter-1) - 2].setEndingBalance(months[0].getEndingBalance());
@@ -27,4 +29,9 @@ void QuarterRecords::addEntry(const LedgerModification& entry) {
     }
     months[entry.getDate().month - 3 * (quarter-1) - 1].addEntry(entry);
     quarterRecords.push_back(entry);
+
+    for(unsigned i = entry.getDate().month - 3 * (quarter-1); i < 3; ++i) { //Prepare future records
+        months[i].setBeginningBalance(months[entry.getDate().month - 3 * (quarter-1) - 1].getEndingBalance());
+        months[i].setEndingBalance(months[entry.getDate().month - 3 * (quarter-1) - 1].getEndingBalance());
+    }
 }
